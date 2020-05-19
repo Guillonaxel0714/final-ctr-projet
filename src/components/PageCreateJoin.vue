d03cd9e32efd8d91fc24efabbc08809a56ea2eec
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2020-05-19 14:14:47 +0200

    add responsive for create/join page

--- src/components/PageCreateJoin.vue
+++ src/components/PageCreateJoin.vue
@@ -58,6 +58,128 @@ export default {
 </script>
 
 <style>
+body{
+  background-image: url("../assets/bgHome.jpg");
+  background-size: cover;
+  background-position: center;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+
+.gauche{
+  height: 100vh;
+}
+
+.droite{
+  height: 100vh;
+}
+
+.responsive-logo{
+  display: none;
+}
+
+.translate{
+  display: none;
+}
+
+.kart img{
+  width: 85%;
+}
+
+.logo{
+  margin: 0 auto;
+}
+
+.kart{
+  width: 90%;
+  padding-top: 2%;
+  position: relative;
+}
+
+.btn{
+  position: absolute;
+  top: 65%;
+}
+
+@media screen and (min-width: 200px) and (max-width: 320px) {
+
+  .logo{
+    display: none;
+  }
+  .responsive-logo{
+    display: block;
+    margin: 0 auto;
+    margin-top: 20%;
+    width: 80%;
+  }
+  .drapeaux{
+    display: none;
+  }
+
+  .droite{
+    height: auto;
+  }
+  .btn{
+    margin-left: 5%;
+  }
+  .kart{
+    margin: 0 auto;
+  }
+
+}
+
+@media screen and (min-width: 321px) and (max-width: 768px) {
+  .logo{
+    display: none;
+  }
+  .responsive-logo{
+    display: block;
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 5%;
+    width: 80%;
+  }
+  .drapeaux{
+    display: none;
+  }
+
+  .droite{
+    height: auto;
+  }
+  .kart{
+    margin: 0 auto;
+    width: 80%;
+    margin-top: 20%;
+    text-align: center;
+  }
+}
+
+@media screen and (min-width: 769px) and (max-width: 1024px) {
+
+  .responsive-logo{
+    display: none;
+  }
+
+  .logo{
+    width: 80%;
+    margin-top: 10%;
+  }
+
+  .kart{
+    margin: 0 auto;
+  }
+
+  .drapeaux{
+    display: none;
+  }
+  .droite{
+    height: auto;
+  }
+  .gauche{
+    height: auto;
+  }
+}
 
 
 </style>
